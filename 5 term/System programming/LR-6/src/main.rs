9f9e913149299ddbeae3609fd9cc21aff1763baf
Artem Bokun <79195348+relsa228@users.noreply.github.com>
2022-10-31 11:55:40 +0300

    string parsers

--- 5 term/System programming/LR-6/src/main.rs
+++ 5 term/System programming/LR-6/src/main.rs
@@ -1,12 +1,62 @@
 use std::net::TcpListener;
 use std::io;
+use std::ops::Range;
+use std::slice::Iter;
 
 
-fn port_is_available(host: &str, port: u16) {
+fn string_validation(wrk_str: String) -> bool {
+    if wrk_str.is_empty() || wrk_str.len() > 5 {
+        return false;
+    }
+    let check: u32 = wrk_str.trim().parse().unwrap();
+    return check <= 65535
+}
+
+fn parsing_input_ports(ports: String) -> Vec<u16> {
+    let mut result_vector: Vec<u16> = Vec::new();
+    let mut buffer_f: String = String::from("");
+    let mut buffer_s: String = String::from("");
+    let mut buffer_change: bool = false;
+    for symb in ports.chars() {
+        if symb.is_numeric() {
+            match !buffer_change {
+                true => buffer_f += symb.encode_utf8(&mut [0; 4]),
+                false => buffer_s += symb.encode_utf8(&mut [0; 4])
+            }
+        }
+        else if symb == ',' || symb == '\n' {
+            let wrk = buffer_f.to_string();
+            match !buffer_change && string_validation(wrk) {
+                true => {
+                    let push_val: u16 = buffer_f.trim().parse().unwrap();
+                    result_vector.push(push_val);
+                }
+                false => {
+                    let mut port_bound: u16 = buffer_s.trim().parse().unwrap();
+                    port_bound += 1;
+                    for val in buffer_f.trim().parse().unwrap()..port_bound {
+                        result_vector.push(val);
+                    };
+                    buffer_change = false;
+                    buffer_s.clear();
+                }
+            }
+            buffer_f.clear();
+        }
+        else if symb == '-' {
+            buffer_change = true;
+        }
+    };
+    return result_vector;
+}
+
+fn port_is_available(host: &str, ports: Vec<u16>) {
     let wrk = host.to_string();
-    match TcpListener::bind((host, port)) {
-        Ok(_) => print!("Port {port} is available, host {wrk}"),
-        Err(_) => print!("Port {port} is busy or unavailable, host {wrk}"),
+    for port in ports {
+        match TcpListener::bind((host, port)) {
+            Ok(_) => println!("Port {port} (host {wrk}) is available"),
+            Err(_) => println!("Port {port} (host {wrk}) is busy or unavailable"),
+        }
     }
 }
 
@@ -18,9 +68,8 @@ fn main() {
     stdin.read_line(&mut user_input_host).unwrap();
 
     println!("Input scanning port: ");
-    let mut user_input_port = String::from("");
-    stdin.read_line(&mut user_input_port).unwrap();
-    let mut user_input_port_int: u16 = user_input_port.trim().parse().unwrap();
+    let mut user_input_ports = String::from("");
+    stdin.read_line(&mut user_input_ports).unwrap();
 
-    port_is_available(user_input_host.trim(), user_input_port_int);
+    port_is_available(user_input_host.trim(), parsing_input_ports(user_input_ports));
 }

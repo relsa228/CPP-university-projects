99056375a7494da4bed766aa51d97b5470ab86ff
Artem Bokun <79195348+relsa228@users.noreply.github.com>
2022-11-07 23:18:34 +0300

    zzzz

--- 5 term/CA/LR-3/main.cpp
+++ 5 term/CA/LR-3/main.cpp
@@ -5,7 +5,9 @@
 #include "omp.h"
 
 int main() {
-    for (int n = 500; n < 2500; n += 500) {
+    std::cout << " | " << "Equations" << " \t| " << "Time" << "  \t| " << std::endl;
+    std::cout << " --------------------------------" << std::endl;
+    for (int n = 500; n <= 2500; n += 500) {
         std::vector<std::vector<double> > matrix(n);
         for (int i = 0; i < n; ++i) {
             matrix[i].resize(n);
@@ -18,10 +20,10 @@ int main() {
 
         //double start_time = omp_get_wtime();                              //based
         //std::vector<double> solution = gauss_solving(matrix, column, n);
-        //std::cout << n << " " << omp_get_wtime() - start_time << std::endl;
+        //std::cout << " | " << n << "  \t| " << omp_get_wtime() - start_time << "  \t| " << std::endl;
 
         double start_time_omp = omp_get_wtime();                            //omp
         std::vector<double> solution_omp = gauss_solving_omp(matrix, column, n);
-        std::cout << n << " " << omp_get_wtime() - start_time_omp << std::endl;
+        std::cout << " | " << n << "  \t| " << omp_get_wtime() - start_time_omp << "  \t| " << std::endl;
     }
 }

435600d490d3d0dbe847219caa7c5e5afac8f17d
Artem Bokun <79195348+relsa228@users.noreply.github.com>
2022-11-11 05:21:03 +0300

    LR-4

--- 5 term/System programming/LR-4/main.cpp
+++ 5 term/System programming/LR-4/main.cpp
@@ -1,99 +1,36 @@
-// C++ program for the above approach
+#include "mono_encr.hpp"
+#include "multithread_encr.hpp"
+#include <chrono>
 
-#include <bits/stdc++.h>
-#include <fstream>
-using namespace std;
-
-// encdec class with encrypt() and
-// decrypt() member functions
-class encdec {
-    int key;
-
-    // File name to be encrypt
-    string file = "test.txt";
-    char c;
-
-public:
-    void encrypt();
-    void decrypt();
-};
-
-// Definition of encryption function
-void encdec::encrypt()
-{
-    // Key to be used for encryption
-    cout << "key: ";
-    //cin >> key;
-    key = 6;
-
-    // Input stream
-    fstream fin, fout;
-
-    // Open input file
-    // ios::binary- reading file
-    // character by character
-    fin.open(file, fstream::in);
-    fout.open("encrypt.txt", fstream::out);
-
-    // Reading original file till
-    // end of file
-    while (fin >> noskipws >> c) {
-        int temp = (c + key);
-
-        // Write temp as char in
-        // output file
-        fout << (char)temp;
-    }
-
-    // Closing both files
-    fin.close();
-    fout.close();
-}
-
-// Definition of decryption function
-void encdec::decrypt()
-{
-    cout << "key: ";
-    //cin >> key;
-    key = 6;
-    fstream fin;
-    fstream fout;
-    fin.open("encrypt.txt", fstream::in);
-    fout.open("decrypt.txt", fstream::out);
-
-    while (fin >> noskipws >> c) {
-
-        // Remove the key from the
-        // character
-        int temp = (c - key);
-        fout << (char)temp;
-    }
-
-    fin.close();
-    fout.close();
-}
-
-// Driver Code
 int main()
 {
-    encdec enc;
-    char c;
-    cout << "\n";
-    cout << "Enter Your Choice : -> \n";
-    cout << "1. encrypt \n";
-    cout << "2. decrypt \n";
-    //cin >> c;
-    c = '2';
-    //cin.ignore();
+    std::fstream fmut;
+    std::string inpt_f, inpt_s;
 
-    switch (c) {
-        case '1': {
-            enc.encrypt();
-            break;
-        }
-        case '2': {
-            enc.decrypt();
-            break;
-        }
-    }
+    fmut.open("test.txt", std::fstream::in);
+    getline( fmut, inpt_f, '\0');
+    fmut.close();
+
+    fmut.open("test.txt", std::fstream::in);
+    getline( fmut, inpt_s, '\0');
+    fmut.close();
+
+
+
+    initMultiDecrypt("multiencr.txt", 4);
+    monoDecrypt("monoencr.txt", 4);
+
+    std::chrono::time_point<std::chrono::system_clock> start_f, end_f, start_s, end_s;
+
+    start_f = std::chrono::system_clock::now();
+    initMultiEncrypt(&inpt_f, "multiencr.txt", 4, 4);
+    end_f = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end_f - start_f;
+    std::cout << "Whith thread: " << elapsed_seconds.count() << "s\n";
+
+    start_s = std::chrono::system_clock::now();
+    monoEncrypt(&inpt_s, "monoencr.txt", 4);
+    end_s = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_s = end_s - start_s;
+    std::cout << "No thread: " << elapsed_seconds_s.count() << "s\n";
 }
